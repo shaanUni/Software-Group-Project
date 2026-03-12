b448458e825ddd46386823ccb07b343ca8ba738f
Shaan Makoon <shaan.makoon@wirebox.co.uk>
2026-03-12 11:30:18 +0000

    mockups folder for front end team

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("apps.pages.urls")),
+    path("mockups/", include("apps.mockups.urls")),
 ]
