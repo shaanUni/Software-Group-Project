49aabff3c8424a90f8d0c5dc83e62755c770663a
tolmir-git <mr.miricteam@gmail.com>
2026-04-23 23:42:46 +0100

    added frames for organisation and teams pages
    Those are just frames for them, they don't do any js and small interaction with python (only with urls etc.), changed the ALLOWED_HOSTS just for easier production PLS CHANGE WHEN AVAILABLE pls pls pls

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g!vz%&h=#rg9q9gbb1f%c8su=y_8=1o-7y$1!*96#ww8m+21=%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*']
 
 
 # Application definition
