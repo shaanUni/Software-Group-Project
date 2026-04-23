893a6946f1b5fade499a7b8075ec733a0ef9e609
Gianluca Cerniglia <cerniglia.gianluca@gmail.com>
2026-04-23 13:56:34 +0100

    fixing urls to have active class on sidebar and messages menu, adding authorship to files

--- apps/team_messages/urls.py
+++ apps/team_messages/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import message_list, message_create, message_detail, message_sent
+from . import views
+
+# This 'app_name' provides the namespace (e.g., 'messaging:message-list')
+app_name = 'messaging'
 
 urlpatterns = [
-    path("messages/", message_list, name="message-list"),
-    path("messages/create/", message_create, name="message-create"),
-    path("messages/sent/", message_sent, name="message-sent"),
-    path("messages/create/<int:team_id>/", message_create, name="message-create-for-team"),
-    path("messages/<int:pk>/", message_detail, name="message-detail"),
+    path("", views.message_list, name="message-list"),
+    path("create/", views.message_create, name="message-create"),
+    path("sent/", views.message_sent, name="message-sent"),
+    path("create/<int:team_id>/", views.message_create, name="message-create-for-team"),
+    path("<int:pk>/", views.message_detail, name="message-detail"),
 ]
--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,6 @@ TEMPLATES = [
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/dashboard/"
 LOGOUT_REDIRECT_URL = "/login/"
-STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 WSGI_APPLICATION = 'config.wsgi.application'
@@ -130,12 +129,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
